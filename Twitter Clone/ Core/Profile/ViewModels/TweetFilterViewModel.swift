4cf35890359616e45e7db1f98585e50e2e691376
Aryan Jagarwal <ajbaggar@gmail.com>
2022-09-20 19:16:45 +0530

    added tweetFilter Bar

---
+++ Twitter Clone/ Core/Profile/ViewModels/TweetFilterViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  TweetFilterViewModel.swift
+//  Twitter Clone
+//
+//  Created by Aryan Jagarwal on 20/09/22.
+//
+
+import Foundation
+
+enum TweetFilterViewModel: Int, CaseIterable {
+    case tweets
+    case replies
+    case likes
+    
+    var title: String {
+        switch self {
+        case.tweets: return "Tweets"
+        case.replies: return "Replies"
+        case.likes: "Likes"
+        }
+        return "Likes"
+    }
+}
